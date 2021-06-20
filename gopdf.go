b1fc1585bc8b5b358c5da607a73a2d33088a377b
omerisildak <omerisildak0@gmail.com>
2021-06-20 12:58:28 +0300

    Small Update

--- gopdf.go
+++ gopdf.go
@@ -12,20 +12,19 @@ import (
 
 func main() {
 
-	file := os.Args[1]
-	content, err := ioutil.ReadFile(file)
-	stringSlience := strings.Split(file, ".")
-	fileName := stringSlience[0]
-	fmt.Println(stringSlience[0])
+	file := os.Args[1]                        //Go takes file as a argv and it start from 1 because Args[0] is go
+	content, err := ioutil.ReadFile(file)     //reading file  and error
+	stringSlience := strings.Split(file, ".") // Spliting filename and its extantion
+	fileName := stringSlience[0]              //to string
 	if err != nil {
 		log.Fatalf("%s file not found", file)
 	}
-	pdf := gofpdf.New("P", "mm", "A4", "")
-	tr := pdf.UnicodeTranslatorFromDescriptor("")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 14)
+	pdf := gofpdf.New("P", "mm", "A4", "")        // here I create a pdf file
+	tr := pdf.UnicodeTranslatorFromDescriptor("") //to convert utf-8 might be we come upon non english words like é ş etc.
+	pdf.AddPage()                                 //adding page
+	pdf.SetFont("Arial", "B", 18)                 //setting font we use Arial as a font
 	pdf.MultiCell(190, 5, tr(string(content)), "0", "0", false)
 	_ = pdf.OutputFileAndClose(fileName + ".pdf")
-	fmt.Printf("%s File Converted to PDF...", fileName)
+	fmt.Printf("%s File Converted to PDF...", fileName) //Message
 
 }
